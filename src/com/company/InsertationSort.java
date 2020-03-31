914f7a5c36b7ef692c164d565e337163b5d7318d
0r0 <0r0.developer@gmail.com>
2020-04-01 03:14:38 +0430

    class with function for doing insertation sort on array ;)

---
+++ src/com/company/InsertationSort.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class InsertationSort {
+    public static double[] done(double[] sortingArray){
+        double key;
+        int j;
+        for(int i=1;i<sortingArray.length;++i){
+            
+            j=i-1;
+            key=sortingArray[i];
+            while(j>=0 && sortingArray[j]>key){
+                sortingArray[j+1]=sortingArray[j];
+                j--;
+            }
+            sortingArray[j+1]=key;
+        }
+        return sortingArray;
+        
+    }
+}
